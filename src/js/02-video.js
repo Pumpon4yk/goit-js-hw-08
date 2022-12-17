721d959446c3f9d470b3436e2683d9d92fd106c8
Pumpon4yk <tt10392@gmail.com>
2022-12-17 17:40:46 +0100

    task02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STOR_KEY = "videoplayer-current-time";
+const VOLUME = "video-volume";
+
+const player = new Player("vimeo-player", {
+    id: 59777392,
+    width: 640
+});
+
+player.on('timeupdate', throttle(onPlay, 1000));
+player.on('volumechange', throttle(onPlayVolume, 1000));
+
+playOnPause()
+
+
+function onPlay(data) {
+    localStorage.setItem(STOR_KEY, JSON.stringify(data))
+};
+function onPlayVolume(data) {
+    localStorage.setItem(VOLUME, JSON.stringify(data))
+};
+
+function playOnPause(){
+    const pause = JSON.parse(localStorage.getItem(STOR_KEY))
+    const soung = JSON.parse(localStorage.getItem(VOLUME))
+
+    player.setCurrentTime(pause.seconds)
+    player.setVolume(soung.volume)
+
+}
